6029438f674fdd629233d2776123424f9cfbe7b6
João Frade <131976573+joaofradept@users.noreply.github.com>
2025-12-04 22:59:20 +0000

    test: image loader height fix

--- assets/js/imageLoader.js
+++ assets/js/imageLoader.js
@@ -1,43 +1,54 @@
 function addImageLoader($img) {
-  // Criar imagem temporária para obter dimensões
-  const tempImg = new Image();
-
-  tempImg.onload = function() {
-    // Agora sabemos as dimensões reais
-    const width = this.width;
-    const height = this.height;
-
-    // Criar placeholder com as dimensões corretas
-    const $spinner = $("<div>")
-    .addClass("img-spinner")
+  // Criar wrapper se não existir
+  if (!$img.parent().hasClass('img-wrapper')) {
+    const $wrapper = $("<div>")
+    .addClass("img-wrapper")
     .css({
-      width: width + 'px',
-      height: height + 'px',
-      display: 'inline-block'
-    })
-    .insertBefore($img);
-
-    $img.hide();
-
-    $img.on("load", function() {
-      $spinner.remove();
-      $(this).css('opacity', 0).show().animate({opacity: 1}, 400);
+      display: 'inline-block',
+      position: 'relative',
+      width: $img.width() || 'auto', // Tentar manter largura
+         height: $img.height() || 'auto'
     });
 
-    // Se já carregou
-    if ($img[0].complete) {
-      setTimeout(() => $img.trigger("load"), 0);
-    }
-  };
+    // Mover estilos dimensionais para o wrapper
+    const imgWidth = $img.attr('width');
+    const imgHeight = $img.attr('height');
 
-  tempImg.src = $img.attr('src');
-}
+    if (imgWidth) $wrapper.css('width', imgWidth + 'px');
+    if (imgHeight) $wrapper.css('height', imgHeight + 'px');
 
-// Uso
-$("img").each(function() {
-  // Só aplicar se não tiver width/height definidos
-  const $img = $(this);
-  if (!$img.attr('width') && !$img.attr('height')) {
-    addImageLoader($img);
+    // Envolver a imagem
+    $img.wrap($wrapper);
+
+    // Adicionar classe de loading à imagem
+    $img.addClass("img-loading");
+
+    // Adicionar spinner ao wrapper
+    $("<div>")
+    .addClass("img-spinner")
+    .appendTo($img.parent());
   }
-});
+
+  $img.on("load", function () {
+    const $this = $(this);
+    const $wrapper = $this.parent();
+
+    // Remover spinner
+    $wrapper.find('.img-spinner').remove();
+
+    // Fazer fade in da imagem
+    $this.removeClass('img-loading').css('opacity', 0);
+    $this.animate({opacity: 1}, 400);
+
+    // Ajustar wrapper ao tamanho natural da imagem
+    $wrapper.css({
+      width: $this.width() + 'px',
+                 height: $this.height() + 'px'
+    });
+  });
+
+  // Se já carregou
+  if ($img[0].complete) {
+    setTimeout(() => $img.trigger("load"), 0);
+  }
+}
