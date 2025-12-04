c05879991219406a2a7ca68d9c2ec8a454b873bb
João Frade <131976573+joaofradept@users.noreply.github.com>
2025-12-04 23:01:58 +0000

    Revert "test: image loader height fix"
    This reverts commit 6029438f674fdd629233d2776123424f9cfbe7b6.

--- assets/js/imageLoader.js
+++ assets/js/imageLoader.js
@@ -1,54 +1,43 @@
 function addImageLoader($img) {
-  // Criar wrapper se não existir
-  if (!$img.parent().hasClass('img-wrapper')) {
-    const $wrapper = $("<div>")
-    .addClass("img-wrapper")
-    .css({
-      display: 'inline-block',
-      position: 'relative',
-      width: $img.width() || 'auto', // Tentar manter largura
-         height: $img.height() || 'auto'
-    });
+  // Criar imagem temporária para obter dimensões
+  const tempImg = new Image();
 
-    // Mover estilos dimensionais para o wrapper
-    const imgWidth = $img.attr('width');
-    const imgHeight = $img.attr('height');
+  tempImg.onload = function() {
+    // Agora sabemos as dimensões reais
+    const width = this.width;
+    const height = this.height;
 
-    if (imgWidth) $wrapper.css('width', imgWidth + 'px');
-    if (imgHeight) $wrapper.css('height', imgHeight + 'px');
-
-    // Envolver a imagem
-    $img.wrap($wrapper);
-
-    // Adicionar classe de loading à imagem
-    $img.addClass("img-loading");
-
-    // Adicionar spinner ao wrapper
-    $("<div>")
+    // Criar placeholder com as dimensões corretas
+    const $spinner = $("<div>")
     .addClass("img-spinner")
-    .appendTo($img.parent());
-  }
+    .css({
+      width: width + 'px',
+      height: height + 'px',
+      display: 'inline-block'
+    })
+    .insertBefore($img);
 
-  $img.on("load", function () {
-    const $this = $(this);
-    const $wrapper = $this.parent();
+    $img.hide();
 
-    // Remover spinner
-    $wrapper.find('.img-spinner').remove();
-
-    // Fazer fade in da imagem
-    $this.removeClass('img-loading').css('opacity', 0);
-    $this.animate({opacity: 1}, 400);
-
-    // Ajustar wrapper ao tamanho natural da imagem
-    $wrapper.css({
-      width: $this.width() + 'px',
-                 height: $this.height() + 'px'
+    $img.on("load", function() {
+      $spinner.remove();
+      $(this).css('opacity', 0).show().animate({opacity: 1}, 400);
     });
-  });
 
-  // Se já carregou
-  if ($img[0].complete) {
-    setTimeout(() => $img.trigger("load"), 0);
-  }
+    // Se já carregou
+    if ($img[0].complete) {
+      setTimeout(() => $img.trigger("load"), 0);
+    }
+  };
+
+  tempImg.src = $img.attr('src');
 }
+
+// Uso
+$("img").each(function() {
+  // Só aplicar se não tiver width/height definidos
+  const $img = $(this);
+  if (!$img.attr('width') && !$img.attr('height')) {
+    addImageLoader($img);
+  }
+});
