309379e9f9421264cdfcfca4a9e4c0bd5383976b
João Frade <131976573+joaofradept@users.noreply.github.com>
2025-12-04 00:52:45 +0000

    Update imageLoader.js

--- assets/js/imageLoader.js
+++ assets/js/imageLoader.js
@@ -20,20 +20,5 @@ function addImageLoader($img) {
 $("img").each(function () {
   const $img = $(this);
 
-  // Cria um spinner/placeholder
-  const $spinner = $("<div>")
-  .addClass("img-spinner")
-  .insertBefore($img);
-
-  $img.css('opacity', 0); // invisível mas ocupa espaço
-
-  $img.on("load", function () {
-    $spinner.remove();   // remove o spinner
-    $img.animate({opacity: 1}, 400); // aparece suavemente
-  });
-
-  // Se a imagem já estiver em cache
-  if ($img[0].complete) {
-    $img.trigger("load");
-  }
+  addImageLoader($img);
 });
